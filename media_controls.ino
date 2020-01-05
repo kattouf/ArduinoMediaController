e31f70ca6d72a13664164a1fd9afee56b36e3d2b
Kattouf <vasiliy.kattouf@gmail.com>
2020-01-05 04:24:41 +0300

    analog volume change

--- media_controls.ino
+++ media_controls.ino
@@ -1,5 +1,4 @@
-#define VOL_DOWN_BUTTON_PIN 13
-#define VOL_UP_BUTTON_PIN 12
+#define VOLUME_POT_PIN A0
 #define PREV_BUTTON_PIN 9
 #define PLAY_BUTTON_PIN 8
 #define NEXT_BUTTON_PIN 7
@@ -7,19 +6,16 @@
 #define PREV_BUTTON_OUTPUT_CODE 101
 #define PLAY_BUTTON_OUTPUT_CODE 102
 #define NEXT_BUTTON_OUTPUT_CODE 103
-#define VOL_DOWN_BUTTON_OUTPUT_CODE 104
-#define VOL_UP_BUTTON_OUTPUT_CODE 105
 
-boolean volDownButtonUp = true;
-boolean volUpButtonUp = true;
+#define VOLUME_POT_VALUE_OUTPUT_START_CODE 200
+#define VOLUME_POT_VALUE_OUTPUT_END_CODE 299
+
+int prevPotValue = 0;
 boolean prevButtonUp = true;
 boolean playButtonUp = true;
 boolean nextButtonUp = true;
 
-
 void setup() {
-  pinMode(VOL_DOWN_BUTTON_PIN, INPUT_PULLUP);
-  pinMode(VOL_UP_BUTTON_PIN, INPUT_PULLUP);
   pinMode(PREV_BUTTON_PIN, INPUT_PULLUP);
   pinMode(PLAY_BUTTON_PIN, INPUT_PULLUP);
   pinMode(NEXT_BUTTON_PIN, INPUT_PULLUP);
@@ -28,14 +24,24 @@ void setup() {
 }
 
 void loop() {
-  handleButtonOnClicks(VOL_DOWN_BUTTON_PIN, &volDownButtonUp);
-  handleButtonOnClicks(VOL_UP_BUTTON_PIN, &volUpButtonUp);
+  handlePotOnChanges(VOLUME_POT_PIN, &prevPotValue, VOLUME_POT_VALUE_OUTPUT_START_CODE, VOLUME_POT_VALUE_OUTPUT_END_CODE);
   handleButtonOnClicks(PREV_BUTTON_PIN, &prevButtonUp);
   handleButtonOnClicks(PLAY_BUTTON_PIN, &playButtonUp);
   handleButtonOnClicks(NEXT_BUTTON_PIN, &nextButtonUp);
 }
 
 // Common
+
+void handlePotOnChanges(int pin, int *prevValue, int startCode, int endCode) {
+  int value = analogRead(VOLUME_POT_PIN);
+  int threshold = 10;
+  if (abs(value - *prevValue) > threshold) {
+    int outputCode = map(value, 0, 1023, startCode, endCode);
+    Serial.print(outputCode);
+
+    *prevValue = value;
+  }
+}
 
 void handleButtonOnClicks(int pin, boolean *wasUp) {
   boolean isUp = digitalRead(pin);
@@ -57,12 +63,6 @@ void buttonClickHandler(int pin) {
   int outputCode;
 
   switch (pin) {
-    case VOL_DOWN_BUTTON_PIN:
-      outputCode = VOL_DOWN_BUTTON_OUTPUT_CODE;
-      break;
-    case VOL_UP_BUTTON_PIN:
-      outputCode = VOL_UP_BUTTON_OUTPUT_CODE;
-      break;
     case PREV_BUTTON_PIN:
       outputCode = PREV_BUTTON_OUTPUT_CODE;
       break;
